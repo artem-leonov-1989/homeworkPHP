e19da931c47d12b8e9514f30d99f3e6c490bbb54
Artem Leonov <artem_leonov@hotmail.com>
2022-01-17 17:12:54 +0200

    Finish dz4

--- auth.php
+++ auth.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
 if (isset($_SESSION['id_user'])) {
-    if ($_SESSION['login_user'] === $_POST['login'] && $_SESSION['password'] === md5($_POST['pass'])) {
+    if ($_SESSION['login_user'] === $_POST['login'] && $_SESSION['password'] == md5($_POST['pass'])) {
         header( 'Location: home.php');
     } else {
-        echo 'ошибка!';
+        echo 'ошибка!<br>';
     }
 } else {
     header( 'Location: reg_form.php');
