f2b28e5ae692ed8d1cb8baa829d8501b7b99fca1
Artem Leonov <artem_leonov@hotmail.com>
2022-01-17 18:52:05 +0200

    ...

--- index.php
+++ index.php
@@ -13,3 +13,13 @@ foreach ($new_arr as $value) {
     fwrite($file , $value);
 }
 fclose($file);
+?>
+<form method="post" enctype="multipart/form-data">
+    <input type="file" name="text_file">
+    <input type="submit" value="Загрузить файл">
+</form>
+<?php
+$uploaddir = '/var/www/html/homeworkPHP/';
+$uploadfile = $uploaddir . basename($_FILES['text_file']['name']);
+move_uploaded_file($_FILES['text_file']['tmp_name'], $uploadfile)
+?>
