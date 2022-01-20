53fcf826fe2998fd654a6b1f90d3d5b756d877f3
Artem Leonov <artem_leonov@hotmail.com>
2022-01-20 20:12:53 +0200

    Finish dz5

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-/*$name_file = 'fructs.txt';
+$name_file = 'fructs.txt';
 $file = fopen($name_file, 'w+');
 $arr = ['Яблоко', 'Груша', 'Виноград', 'Вишня', 'Абрикос', 'Слива', 'Персик', 'Черешня'];
 foreach ($arr as $value) {
@@ -12,7 +12,7 @@ $file = fopen($name_file, 'w+');
 foreach ($new_arr as $value) {
     fwrite($file , $value);
 }
-fclose($file);*/
+fclose($file);
 ?>
 <form method="post" enctype="multipart/form-data">
     <input type="file" name="text_file">
@@ -28,4 +28,35 @@ if (isset($_POST['upload'])) {
         readfile($_FILES['text_file']['tmp_name']);
     }
 }
+?>
+<form method="post">
+    <input type="text" name="name_user" placeholder="Ваше имя">
+    <input type="text" name="login_user" placeholder="Придумайте логин">
+    <input type="password" name="pass_user" placeholder="Придумайте пароль">
+    <input type="password" name="repeat_pass" placeholder="Повторите пароль">
+    <input type="submit" name="reg_user" value="Зарегиться">
+</form>
+<?php
+if (isset($_POST['reg_user'])) {
+    if (strlen($_POST['pass_user']) != 0 && $_POST['pass_user'] === $_POST['repeat_pass'] && strlen($_POST['name_user']) != 0 && strlen($_POST['login_user']) != 0) {
+        $user_file_name = 'user_info.txt';
+        $user_file = fopen($user_file_name, 'w+');
+        fwrite($user_file, $_POST['name_user'].PHP_EOL);
+        fwrite($user_file, $_POST['login_user']);
+        fclose($user_file);
+    }
+}
+function del_dir($path) {
+    $path = __DIR__.'\\'.$path;
+    $del_files = scandir($path);
+    var_dump($del_files);
+    if ($del_files) {
+        foreach ($del_files as $target) {
+            if ($target !== '.' && $target !== '..')
+            unlink($path.'\\'.$target);
+        }
+        rmdir($path);
+    }
+}
+/*del_dir('Torrent-file');*/
 ?>
